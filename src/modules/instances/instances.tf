9ea251f5a8555e16cf33b083d6d5b2018adefa6b
cloudstones <70410709+cloudstones@users.noreply.github.com>
2021-06-04 21:38:53 +0530

    Update instances.tf

--- src/modules/instances/instances.tf
+++ src/modules/instances/instances.tf
@@ -12,17 +12,17 @@ data "template_file" "webserver-userdata" {
 
 
 
-################################################  web server #########################
-resource "aws_instance" "webserver" {
+################################################  lb server #########################
+resource "aws_instance" "lbserver" {
 ami = "${var.myamiid}"
 instance_type = "t2.medium"
-subnet_id = "${aws_subnet.publicsubnet.id}"
+subnet_id = "${aws_subnet.publicsubnet1.id}"
 private_ip = "192.168.1.6"
 vpc_security_group_ids = ["${aws_security_group.websg.id}"]
 key_name = "${var.mykeypair}"
 user_data = "${data.template_file.webserver-userdata.rendered}"
 tags = {
-Name = "webserver"
+Name = "lbserver"
 }
 }
 
@@ -30,7 +30,7 @@ Name = "webserver"
 resource "aws_instance" "appserver" {
 ami = "${var.myamiid}"
 instance_type = "t2.medium"
-subnet_id = "${aws_subnet.publicsubnet.id}"
+subnet_id = "${aws_subnet.publicsubnet1.id}"
 private_ip = "192.168.1.7"
 vpc_security_group_ids = ["${aws_security_group.websg.id}"]
 key_name = "${var.mykeypair}"
@@ -43,7 +43,7 @@ Name = "appserver"
 resource "aws_instance" "dbserver" {
 ami = "${var.myamiid}"
 instance_type = "t2.medium"
-subnet_id = "${aws_subnet.publicsubnet.id}"
+subnet_id = "${aws_subnet.publicsubnet1.id}"
 private_ip = "192.168.1.8"
 vpc_security_group_ids = ["${aws_security_group.websg.id}"]
 key_name = "${var.mykeypair}"
@@ -79,30 +79,58 @@ Name = "myigw"
 }
 
 
-resource "aws_subnet" "publicsubnet"{
+resource "aws_subnet" "publicsubnet1"{
 vpc_id = "${aws_vpc.myvpc.id}"
 cidr_block = "192.168.1.0/24"
 tags={
-Name = "publicsubnet"
+Name = "publicsubnet1"
 }
 }
 
-resource "aws_route_table" "publicrtb"{
+resource "aws_route_table" "publicrtb1"{
 vpc_id = "${aws_vpc.myvpc.id}"
 tags = {
-Name = "publicrtb"
+Name = "publicrtb1"
 }
 }
 
-resource "aws_route" "publicrt"{
-route_table_id = "${aws_route_table.publicrtb.id}"
+resource "aws_route" "publicrt1"{
+route_table_id = "${aws_route_table.publicrtb1.id}"
 destination_cidr_block = "0.0.0.0/0"
 gateway_id = "${aws_internet_gateway.myigw.id}"
 }
  
-resource "aws_route_table_association" "publicrtba"{
-route_table_id = "${aws_route_table.publicrtb.id}"
-subnet_id = "${aws_subnet.publicsubnet.id}"
+resource "aws_route_table_association" "publicrtba1"{
+route_table_id = "${aws_route_table.publicrtb1.id}"
+subnet_id = "${aws_subnet.publicsubnet1.id}"
+}
+
+
+
+resource "aws_subnet" "publicsubnet2"{
+vpc_id = "${aws_vpc.myvpc.id}"
+cidr_block = "192.168.2.0/24"
+tags={
+Name = "publicsubnet2"
+}
+}
+
+resource "aws_route_table" "publicrtb2"{
+vpc_id = "${aws_vpc.myvpc.id}"
+tags = {
+Name = "publicrtb2"
+}
+}
+
+resource "aws_route" "publicrt2"{
+route_table_id = "${aws_route_table.publicrtb2.id}"
+destination_cidr_block = "0.0.0.0/0"
+gateway_id = "${aws_internet_gateway.myigw.id}"
+}
+ 
+resource "aws_route_table_association" "publicrtba2"{
+route_table_id = "${aws_route_table.publicrtb2.id}"
+subnet_id = "${aws_subnet.publicsubnet2.id}"
 }
 
 ##############################################  Security Modules ########################
